c93fe054ff0108f2d189275f9073221bc7597f7e
lenin fernando <ingeleninfernando@gmail.com>
2017-01-10 15:22:54 -0500

    implemmenta la funcionalidad de votos

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,16 +1,18 @@
 # == Schema Information
 #
-# Table name: comments
+# Table name: votes
 #
 #  id         :integer          not null, primary key
-#  body       :text
-#  user_id    :integer
 #  product_id :integer
+#  user_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
+require 'test_helper'
+
+class VoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
 end
